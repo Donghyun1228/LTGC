33d74dba27aa5c1dea9c0284592c37793fcb0991
Donghyun1228 <dawnme1228@naver.com>
2025-02-27 16:49:05 +0900

    model: replace gpt-4 with llama

--- llm_extension.py
+++ llm_extension.py
@@ -1,9 +1,12 @@
-from openai import OpenAI
+#%%
+# from openai import OpenAI
 import pandas as pd
+import torch
+from transformers import MllamaForConditionalGeneration, AutoProcessor
 from data_txt.imagenet_label_mapping import get_readable_name
 import csv
 import argparse
-
+# %%
 # hyper-param
 parser = argparse.ArgumentParser(description='PyTorch Training')
 
@@ -15,9 +18,18 @@ parser.add_argument('-ext', '--extended_description_path', default='descriptions
                     help='File path to the extended description file')
 args = parser.parse_args()
 
-api_key = "Replace with your own OPENAI KEY."
+# api_key = "Replace with your own OPENAI KEY."
 
-client = OpenAI(api_key=api_key)
+# client = OpenAI(api_key=api_key)
+# %%
+model_id = "meta-llama/Llama-3.2-11B-Vision-Instruct"
+
+model = MllamaForConditionalGeneration.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+)
+processor = AutoProcessor.from_pretrained(model_id)
 
 df = pd.read_csv(args.existing_description_path, header=None, names=['label', 'text'])
 grouped_texts = df.groupby('label')['text'].apply(lambda x: '\n'.join(x)).to_dict()
@@ -38,18 +50,34 @@ for label, text in grouped_texts.items():
       ### self-reflection
       user_content = "Besides these descriptions mentioned above, please use the same Template to list other possible {distinctive features} and {specific scenes} for the class " + real_name
 
-      completion = client.chat.completions.create(
-        # model="gpt-3.5-turbo",
-        model="gpt-4-turbo-preview",
-        messages=[
-          {"role": "system", "content": system_content},
-          {"role": "user", "content": current_description},
-          {"role": "user", "content": user_content}
-        ]
-      )
+      # completion = client.chat.completions.create(
+      #   # model="gpt-3.5-turbo",
+      #   model="gpt-4-turbo-preview",
+      #   messages=[
+      #     {"role": "system", "content": system_content},
+      #     {"role": "user", "content": current_description},
+      #     {"role": "user", "content": user_content}
+      #   ]
+      # )
 
-      output = completion.choices[0].message.content
+      # output = completion.choices[0].message.content
       # sentences = output.split(". ")
+
+      messages = [
+        {"role": "system", "content": system_content},
+        {"role": "user", "content": current_description},
+        {"role": "user", "content": user_content}
+      ]
+      input_text = processor.apply_chat_template(messages, add_generation_prompt=True)
+      inputs = processor(
+          input_text,
+          add_special_tokens=False,
+          return_tensors="pt"
+      ).to(model.device)
+
+      output = model.generate(**inputs, max_new_tokens=0)
+      output = processor.decode(output[0])
+
       if '\n\n- ' in output:
         sentences = output.split("\n\n")
       elif '\n\n' in output:
@@ -68,3 +96,5 @@ for label, text in grouped_texts.items():
             writer.writerow([label, s])
 
       # print(completion.choices[0].message.content)
+
+# %%
